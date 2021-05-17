490b433f453a40aa73ec5f101aec51508666ec6f
BugbearR <bugbearr@bugbearr.jp>
2021-05-17 06:14:21 +0000

    Add unlink experiment

---
+++ fs/unlink-exp/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+    int subResult;
+    if (argc < 2) {
+        fprintf(stderr, "usage: %s path\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+    subResult = unlink(argv[1]);
+    if (subResult != 0) {
+        int subErrno = errno;
+        perror("unlink");
+        fprintf(stderr, "errno:%d\n", subErrno);
+        return EXIT_FAILURE;
+    }
+    return EXIT_SUCCESS;
+}
