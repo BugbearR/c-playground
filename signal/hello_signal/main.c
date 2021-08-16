ea6b656a6cecceeca46c377a77fef675273f2485
BugbearR <bugbearr@gmail.com>
2021-08-17 08:26:07 +0900

    Add busy loop

--- signal/hello_signal/main.c
+++ signal/hello_signal/main.c
@@ -1,6 +1,40 @@
+#define _POSIX_C_SOURCE 199309L
+
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <time.h>
+#include <inttypes.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+
+
+volatile sig_atomic_t g_sigNo;
+volatile sig_atomic_t g_serial;
+
+void mySignalHandler(int sigNo, siginfo_t *pInfo, void *pCtx)
+{
+    char fileName[32];
+    char buf[128];
+    int fd = -1;
+    struct timespec ts;
+
+    g_sigNo = sigNo;
+    clock_gettime(CLOCK_REALTIME, &ts);
+    snprintf(fileName, sizeof(buf), "signal_%010d", (int)g_serial);
+    g_serial++;
+    fd = open(fileName, O_CREAT | O_WRONLY, S_IRUSR);
+    snprintf(buf, sizeof(buf), "%" PRIdMAX ".%09ld\n", (intmax_t)ts.tv_sec, ts.tv_nsec);
+    write(fd, buf, strlen(buf));
+EXIT_FUNC:
+    if (fd != -1) {
+        close(fd);
+    }
+}
 
 int main(void)
 {
@@ -8,6 +42,26 @@ int main(void)
     int subResult;
     int subErrno;
     int count;
+    int i;
+    int s;
+    struct sigaction sa = {0};
+    // struct sigaction oldSa1 = {0};
+    // struct sigaction oldSa2 = {0};
+
+    sa.sa_sigaction = mySignalHandler;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = SA_SIGINFO;
+
+    if (sigaction(SIGINT, &sa, NULL) == -1)
+    {
+        perror("sigaction");
+        exit(EXIT_FAILURE);
+    }
+    if (sigaction(SIGUSR1, &sa, NULL) == -1)
+    {
+        perror("sigaction");
+        exit(EXIT_FAILURE);
+    }
 
     for (count = 0; ; count++)
     {
@@ -24,10 +78,18 @@ int main(void)
             subErrno = errno;
             if (subErrno == EINTR)
             {
-                printf("EINTR in sleep.");
+                printf("EINTR in sleep.\n");
+                printf("g_sigNo:%d\n", (int)g_sigNo);
             }
         }
         printf("busy loop\n");
+        s = 0;
+        for (i = 0; i < 1000000000; i++)
+        {
+            s += rand();
+        }
+        printf("s:%d\n", s);
+        printf("busy loop end\n");
     }
 
 EXIT_FUNC:
