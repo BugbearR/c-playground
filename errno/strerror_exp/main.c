aaac8a07fefb6af5ab7e48419d189a6df2218130
BugbearR <bugbearr@bugbearr.jp>
2021-10-19 00:14:28 +0000

    Add usage

--- errno/strerror_exp/main.c
+++ errno/strerror_exp/main.c
@@ -4,6 +4,11 @@
 
 int main(int argc, char *argv[])
 {
+    if (argc < 2) {
+        fprintf(stderr, "usage: %s number\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
     int myErrno = atoi(argv[1]);
     printf("%s\n", strerror(myErrno));
     return EXIT_SUCCESS;
