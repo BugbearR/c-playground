418f5ea8b5f09349a10332c51fe5b406abaa8993
BugbearR <bugbearr@bugbearr.jp>
2021-10-26 10:26:54 +0900

    Improve string/sprintf_exp

--- string/sprintf_exp/main.c
+++ string/sprintf_exp/main.c
@@ -2,22 +2,52 @@
 #include <stdlib.h>
 #include <memory.h>
 
+void dump(char *p, size_t len) {
+    size_t i;
+    for (i = 0; i < len; i++)
+    {
+        printf(" %02hhx", p[i]);
+    }
+}
+
+void test(size_t dstLen, const char *format, const char *s)
+{
+    char *dst;
+    int r;
+
+    dst = malloc(dstLen);
+
+    memset(dst, 'X', dstLen);
+
+    printf("&dst[0]: %p\n", &dst[0]);
+
+    printf("sprintf(dst, \"%s\", \"%s\");\n", format, s);
+    r = sprintf(dst, format, s);
+    printf("return: %d\n", r);
+    printf("dst:");
+    dump(dst, dstLen);
+    printf("\n");
+    printf("\n");
+
+    free(dst);
+}
+
 int main(void)
 {
-    char dstBuf[6];
-    char srcBuf[4];
-    int i;
+    test(5, "%-2.2s", "");
+    test(5, "%-2.2s", "A");
+    test(5, "%-2.2s", "AB");
+    test(5, "%-2.2s", "ABC");
 
-    sprintf(dstBuf, "XXXXX");
-    sprintf(srcBuf, "A");
+    test(5, "%2.2s", "");
+    test(5, "%2.2s", "A");
+    test(5, "%2.2s", "AB");
+    test(5, "%2.2s", "ABC");
 
-    // sprintf(dstBuf, "%.2s", srcBuf);
-    sprintf(dstBuf, "%-2.2s", srcBuf);
-
-    for (i = 0; i < sizeof(dstBuf); i++)
-    {
-        printf("%02x ", dstBuf[i]);
-    }
+    test(5, "%.2s", "");
+    test(5, "%.2s", "A");
+    test(5, "%.2s", "AB");
+    test(5, "%.2s", "ABC");
 
     return EXIT_SUCCESS;
 }
