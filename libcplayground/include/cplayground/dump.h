517d2171fa483503a5264d337ce0f61bb0e6b59f
BugbearR <bugbearr@bugbearr.jp>
2022-03-18 10:15:02 +0900

    Raname

--- libcplayground/include/cplayground/dump.h
+++ libcplayground/include/cplayground/dump.h
@@ -1,6 +1,10 @@
 #ifndef CPLAYGROUND_DUMP_H_
 #define CPLAYGROUND_DUMP_H_
 
+#ifdef HAVE_PRAGMA_ONCE
+#pragma once
+#endif
+
 #include <stddef.h>
 
 void cplayground_dump(const void *pBuf, size_t bufLen);
