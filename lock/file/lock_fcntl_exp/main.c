fc113792fd44b9ee47383dc0e244606851c690ab
BugbearR <bugbearr@bugbearr.jp>
2025-04-03 01:27:24 +0000

    Implove lock_fcntl_exp

--- lock/file/lock_fcntl_exp/main.c
+++ lock/file/lock_fcntl_exp/main.c
@@ -5,23 +5,40 @@
 
 int main(int argc, char *argv[]) {
     int fd;
+    int repeatCount = 1;
     int start = 0;
     int length = 0;
     struct flock lock;
-    char buf[32];
+    char *buf;
     int ret;
+    int i;
+    int j;
+    int k;
+    pid_t pid;
 
     if (argc < 2) {
-        fprintf(stderr, "Usage: %s <file> [start] [length]\n", argv[0]);
+        fprintf(stderr, "Usage: %s <file> [repeatCount] [start] [length]\n", argv[0]);
         exit(1);
     }
 
     if (argc > 2) {
-        start = atoi(argv[2]);
+        repeatCount = atoi(argv[2]);
     }
 
     if (argc > 3) {
-        length = atoi(argv[3]);
+        start = atoi(argv[3]);
+    }
+
+    if (argc > 4) {
+        length = atoi(argv[4]);
+    }
+
+    pid = getpid();
+
+    buf = malloc(length);
+    if (buf == NULL) {
+        perror("malloc");
+        exit(1);
     }
 
     fd = open(argv[1], O_RDWR | O_CREAT, 0644);
@@ -32,28 +49,62 @@ int main(int argc, char *argv[]) {
 
     printf("Opened\n");
 
-    lock.l_type = F_WRLCK;
-    lock.l_whence = SEEK_SET;
-    lock.l_start = start;
-    lock.l_len = length;
-    ret = fcntl(fd, F_SETLKW, &lock);
-    if (ret < 0) {
-        perror("fcntl");
-        exit(1);
+    for (k = 0; k < repeatCount; k++) {
+        lock.l_type = F_WRLCK;
+        lock.l_whence = SEEK_SET;
+        lock.l_start = start;
+        lock.l_len = length;
+        ret = fcntl(fd, F_SETLKW, &lock);
+        if (ret < 0) {
+            perror("fcntl");
+            exit(1);
+        }
+
+        printf("pid: %d, Locked\n", pid);
+
+        for (j = 0; j < 3; j++) {
+            off_t newOff = lseek(fd, start, SEEK_SET);
+            if (newOff < 0) {
+                perror("lseek");
+                exit(1);
+            }
+
+            ret = read(fd, buf, length);
+            if (ret < 0) {
+                perror("read");
+                exit(1);
+            }
+            printf("pid: %d, read buf:[%.*s]\n", pid, length, buf);
+
+            for (i = 0; i < length; i++) {
+                buf[i] = 'a' + ((i + j + k + pid) % 26);
+            }
+
+            printf("pid: %d, write buf:[%.*s]\n", pid, length, buf);
+            newOff = lseek(fd, start, SEEK_SET);
+            if (newOff < 0) {
+                perror("lseek");
+                exit(1);
+            }
+            ret = write(fd, buf, length);
+            if (ret < 0) {
+                perror("write");
+                exit(1);
+            }
+            sleep(1);
+        }
+
+        lock.l_type = F_UNLCK;
+        ret = fcntl(fd, F_SETLK, &lock);
+        if (ret < 0) {
+            perror("fcntl");
+            exit(1);
+        }
+
+        printf("pid: %d, Unlocked\n", pid);
+
+        sleep(1);
     }
-
-    printf("Locked\n");
-
-    sleep(10);
-
-    lock.l_type = F_UNLCK;
-    ret = fcntl(fd, F_SETLK, &lock);
-    if (ret < 0) {
-        perror("fcntl");
-        exit(1);
-    }
-
-    printf("Unlocked\n");
 
     close(fd);
 
